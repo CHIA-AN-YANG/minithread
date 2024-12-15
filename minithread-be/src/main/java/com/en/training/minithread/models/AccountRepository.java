f4b8e24e27c1c04c54a51d5ef589f746dabf2ec4
Anna Yang <anna.yang@seidor.com>
2024-12-15 22:15:03 +0800

    bugfix fix type error

--- minithread-be/src/main/java/com/en/training/minithread/models/AccountRepository.java
+++ minithread-be/src/main/java/com/en/training/minithread/models/AccountRepository.java
@@ -3,8 +3,9 @@ package com.en.training.minithread.models;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface AccountRepository extends JpaRepository<Account, Long> {
+public interface AccountRepository extends JpaRepository<Account, UUID> {
     // JPA will provide the class
     Optional<Account> findByUsername(String username);
 
